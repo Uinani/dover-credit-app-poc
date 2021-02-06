037104ad01a765734b7a173708c192d5c11c905c
Jose <jose.e.chavez@gmail.com>
2021-02-06 09:12:12 -0600

    Unit tests added

--- src/app/components/address/address.component.spec.ts
+++ src/app/components/address/address.component.spec.ts
@@ -1,21 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AddressComponent } from './address.component';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { creditApplicationFormGroup } from 'src/app/common/credit-application.form';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 describe('AddressComponent', () => {
   let component: AddressComponent;
   let fixture: ComponentFixture<AddressComponent>;
+  let formGroup: FormGroup;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddressComponent ]
+      imports: [
+        FlexLayoutModule,
+        FormsModule,
+        MatInputModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+      ],
+      declarations: [AddressComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
+    formGroup = creditApplicationFormGroup;
     fixture = TestBed.createComponent(AddressComponent);
     component = fixture.componentInstance;
+    component.formGroup = formGroup;
     fixture.detectChanges();
   });
 
